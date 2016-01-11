9843a369dd3ce463934bc8fd28a440e58cca540d
pushkar007 <pushkar.mishra@cuefinsolutions.com>
2016-01-11 06:36:23 +0000

    Added recipe listing and show recipe.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  get '/', to: "pages#home"
+  root "pages#home"
+  get '/home', to: "pages#home"
+  
+  # get '/recipes', to: 'recipes/#index'
+  # get '/recipes/new', to: 'recipes/#new', as: 'new_recipe'
+  # post '/recipes', to: 'recipes/#create'
+  # get '/recipes/:id/edit', to: 'recipes/#edit', as: 'edit_recipe'
+  # patch '/recipes/:id', to: 'recipes/#update'
+  # get '/recipes/:id', to: 'recipes/#show', as: 'recipe'
+  # delete '/recipes/:id', to: 'recipes/#distroy'
+  
+  resources :recipes
+  
+  
 end
