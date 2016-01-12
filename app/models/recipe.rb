3e13a47751aee5d47f1c3f105138cb4db6b07875
pushkar007 <pushkar.mishra@cuefinsolutions.com>
2016-01-12 07:05:15 +0000

    Added picture colum in recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 5, maximum: 100}
   validates :summary, presence: true , length: {minimum: 20, maximum: 150}
   validates :description, presence: true, length: {minimum: 50, maximum: 500}
+  
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size
+      if picture_size > 5.megabytes
+        errors.add(:picture, "should be less than 5 MB")
+      end
+    end
 end
